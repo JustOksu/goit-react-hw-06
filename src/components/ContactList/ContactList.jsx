a851929c59cc681a107240805afd0c94b38f69f0
Osadchyi Yurii <osa26091994@icloud.com>
2024-12-03 10:20:32 +0200

    delete comments

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,14 +1,13 @@
 import { useSelector } from "react-redux";
 import { selectContacts } from "../../redux/contactsSlice";
-import { selectNameFilter } from "../../redux/filtersSlice"; // Импортируем фильтр
+import { selectNameFilter } from "../../redux/filtersSlice";
 import Contact from "../Contact/Contact";
 import styles from "./ContactList.module.css";
 
 const ContactList = () => {
-  const contacts = useSelector(selectContacts); // Все контакты из состояния
-  const filter = useSelector(selectNameFilter); // Фильтр имени
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
 
-  // Фильтруем контакты по имени
   const filteredContacts = contacts.filter((contact) =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
   );
